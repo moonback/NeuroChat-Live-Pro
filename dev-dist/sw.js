77a036004be7e15bcd9b44e8b5a454a7a9bc0268
Medusa <devoye.maysson@gmail.com>
2025-12-28 18:58:40 +0100

    Update service worker cache revision for index.html to ensure proper asset management and cache invalidation.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-a1974cce'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ne9ttl6t988"
+    "revision": "0.ouilqc1lhr8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
