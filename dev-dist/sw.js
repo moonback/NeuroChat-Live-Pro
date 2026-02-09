f7ba40dbbc0add0d1e74347f506df7d8a72d65f9
Medusa <devoye.maysson@gmail.com>
2026-02-09 17:35:03 +0100

    up

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-a1974cce'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.hb7fb8g0kg"
+    "revision": "0.kjfuukipa2o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
